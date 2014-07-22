70e820df2d7a8fb545bbbe6315a12d5d6689166e
Walker Randolph Smith <walker@wearesoftwarecraftsmen.com>
2014-07-21 22:55:28 -0400

    added scrolly and classie.

--- app/scripts/controllers/homeController.js
+++ app/scripts/controllers/homeController.js
@@ -1,7 +1,7 @@
 define([], function() {
     return ['$scope', '$http', function($scope, $http) {
         // You can access the scope of the controller from here
-
+        console.log(new cbpScroller( document.getElementById( 'cbp-so-scroller' ) ));
         $scope.$apply();
     }];
 });
