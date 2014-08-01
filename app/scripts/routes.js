c4578c637431d1b73ed2987a86869cb3ea71059b
Walker Randolph Smith <walker@wearesoftwarecraftsmen.com>
2014-08-01 11:27:37 -0400

    Updated reminder frequency.

--- app/scripts/routes.js
+++ app/scripts/routes.js
@@ -34,9 +34,21 @@ define(['angular', 'app'], function(angular, app) {
             },{
 
             });
+            var second = 1000;
+            var minute = 6000; //60 * second;
+            var quarter_hour = 90000; //15 * minute
+            var half_hour = 180000; //30 * minute
+            var hour = 360000; //60 * minute;
+            var half_day = 4320000; //12 * hour
+            var day = 8640000; //24 * hour;
+            var week = 60480000; //7 * day;
+
+            $rootScope.milli = [ quarter_hour, half_hour, hour, hour*2, hour*3, hour*4, hour*6, hour*8, hour*10, half_day, hour*15, hour*18, hour*20, hour*21, hour*22, day, day*2, day*3, day*5, week, 0 ];
+            $rootScope.phrases = [ "15 minutes", "30 minutes", "1 hour", "2 hours", "3 hours","4 hours", "6 hours","8 hours", "10 hours","12 hours", "15 hours","18 hours", "20 hours","21 hours", "22 hours", "Day", "2 Days", "3 Days", "5 Days", "Week", "Unlimited"];
 
             $rootScope.isApp = true;
-
+            $rootScope.data = {};
+            $rootScope.data.numberSelection = ($rootScope.sessionUser != null ? $rootScope.sessionUser.frequency() : 0);
             $rootScope.sessionUser = User.current();
 
             $rootScope.logOut = function(form) {
@@ -64,7 +76,7 @@ define(['angular', 'app'], function(angular, app) {
                 $rootScope.sessionUser.set('receivePushNotifications', result);
                 $rootScope.sessionUser.save(null, {
                    success: function(){
-
+                       console.log("Updated Receive Push Notifications to: " + result);
                    }
                 });
             }
@@ -75,7 +87,7 @@ define(['angular', 'app'], function(angular, app) {
                 $rootScope.sessionUser.set('receiveEmails', result);
                 $rootScope.sessionUser.save(null, {
                     success: function(){
-
+                        console.log("Updated Receive Emails to: " + result);
                     }
                 });
             }
@@ -86,13 +98,21 @@ define(['angular', 'app'], function(angular, app) {
                 $rootScope.sessionUser.set('receiveSMS', result);
                 $rootScope.sessionUser.save(null, {
                     success: function(){
-
+                        console.log("Updated Receive SMS to: " + result);
                     }
                 });
             }
 
-            $rootScope.data = {};
-            $rootScope.data.numberSelection = ($rootScope.sessionUser != null ? $rootScope.sessionUser.frequency() : 0);
+            $rootScope.setFrequency = function(value){
+                value = parseInt(value);
+                $rootScope.sessionUser.set('frequency', value);
+                $rootScope.sessionUser.save(null, {
+                    success: function() {
+                        console.log("Updated Reminder Frequency.");
+                    }
+                });
+            }
+
 
         })
 });
