911231ee03ce3f4080893097b65e83f8ca7a6547
Walker Randolph Smith <walker@wearesoftwarecraftsmen.com>
2014-08-03 19:22:29 -0400

    Moved Parse User extension to a service YoloUser.

--- app/scripts/routes.js
+++ app/scripts/routes.js
@@ -17,25 +17,9 @@ define(['angular', 'app'], function(angular, app) {
         });
         $routeProvider.otherwise({redirectTo: '/home'});
     }])
-        .run(function($rootScope){
+        .run(function($rootScope, YoloUser){
             Parse.initialize("yG0OKddCMctN5vtCj5ocUbDxrRJjlPuzZLXMOXA9", "MgdbUbWiTaPbuZOp2N4rMsON7av9ITWvzSC0qiuV");
-
-            var User = Parse.User.extend({
-                username: function (){ return this.get('username') },
-                password: function (){ return this.get('password') },
-                email: function (){ return this.get('email') },
-                phone: function (){ return this.get('phone') },
-                children: function (){ return this.get('children') },
-                receivePushNotifications: function (){ return this.get('receivePushNotifications'); },
-                receiveEmails: function (){ return this.get('receiveEmails'); },
-                receiveSMS: function (){ return this.get('receiveSMS'); },
-                frequency: function () { return this.get('frequency'); }
-
-            },{
-
-            });
-
-            $rootScope.sessionUser = User.current();
+            $rootScope.sessionUser = Parse.User.current();
 
             $rootScope.milli = [ 90000, 180000, 360000, 360000*2, 360000*3, 360000*4, 360000*6, 360000*8, 360000*10, 4320000, 360000*15, 360000*18, 360000*20, 360000*21, 360000*22, 8640000, 8640000*2, 8640000*3, 8640000*5, 60480000, 0 ];
             $rootScope.phrases = [ "15 minutes", "30 minutes", "1 hour", "2 hours", "3 hours","4 hours", "6 hours","8 hours", "10 hours","12 hours", "15 hours","18 hours", "20 hours","21 hours", "22 hours", "Day", "2 Days", "3 Days", "5 Days", "Week", "Unlimited"];
--- app/scripts/services.js
+++ app/scripts/services.js
@@ -7,5 +7,28 @@ define(['angular'], function (angular) {
     // Demonstrate how to register services
     // In this case it is a simple value service.
     angular.module('YoloApp.services', [])
-        .value('version', '0.1');
+        .value('version', '0.1')
+        .factory('YoloUser', function($q){
+
+            var User = Parse.User.extend({
+                username: function (){ return this.get('username') },
+                password: function (){ return this.get('password') },
+                email: function (){ return this.get('email') },
+                phone: function (){ return this.get('phone') },
+                children: function (){
+                    return this.get('children')
+                    //var defer = $q.defer();
+                    //defer.resolve(this.get('children'));
+                    //return defer.promise;
+                },
+                receivePushNotifications: function (){ return this.get('receivePushNotifications'); },
+                receiveEmails: function (){ return this.get('receiveEmails'); },
+                receiveSMS: function (){ return this.get('receiveSMS'); },
+                frequency: function () { return this.get('frequency'); }
+            },{
+
+            });
+
+            return User;
+        });
 });
